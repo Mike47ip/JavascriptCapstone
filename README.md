8cd5d940529ae1f529daf0a631a155f67e90b03c
Mike Dark <agyemankojo47@gmail.com>
2023-07-05 17:34:03 +0000

    Add Javascript DOM code

--- README.md
+++ README.md
@@ -1,1 +1,126 @@
-# JavascriptCapstone
+# Architectural-Summit
+
+
+<!-- TABLE OF CONTENTS -->
+
+# 📗 Table of Contents
+
+- [📖 About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+  - [🚀 Live Demo](#live-demo)
+- [💻 Getting Started](#getting-started)
+  - [Setup](#setup)
+  - [Prerequisites](#prerequisites)
+  - [Install](#install)
+- [👥 Authors](#authors)
+- [🔭 Future Features](#future-features)
+- [🤝 Contributing](#contributing)
+- [⭐️ Show your support](#support)
+- [🙏 Acknowledgements](#acknowledgements)
+- [📝 License](#license)
+
+<!-- PROJECT DESCRIPTION -->
+
+# 📖 [Architectural - Summit] <a name="about-project"></a>
+
+**[Global Leaders' Conference]** is a capstone project done at the end of the each of the Microverse Core Curriculum modules. It is similar to real-world project and it is built with business specifications that will look really nice in a portfolio.
+
+## 🛠 Built With <a name="built-with"></a>
+
+### Tech Stack <a name="tech-stack"></a>
+
+<details>
+  <summary>HTML, CSS</summary>
+  <ul>
+    <li><a href="https://reactjs.org/">HTML, CSS</a></li>
+  </ul>
+</details>
+
+<details>
+  <summary>Github flow</summary>
+  <ul>
+    <li><a href="https://docs.github.com/en/get-started/quickstart/github-flow">Github flow</a></li>
+  </ul>
+</details>
+
+<details>
+<summary>VScode</summary>
+  <ul>
+    <li><a href="https://code.visualstudio.com/">VScode</a></li>
+  </ul>
+</details>
+
+<!-- LIVE DEMO -->
+
+## 🚀 Live Demo <a name="live-demo"></a>
+
+- [Architectural-Summit](https://kwaku28.github.io/architectural-summit/)
+
+## 🚀 Video Presentation <a name="video-presentation"></a>
+
+- [Loom video](https://www.loom.com/share/4cdfdf9375f54de1a96bdc3ea8a95420)
+
+<!-- GETTING STARTED -->
+
+## 💻 Getting Started <a name="getting-started"></a>
+
+To get a local copy up and running, follow these steps.
+
+### Prerequisites
+
+In order to run this project you need:
+
+Install a code editor
+
+### Setup
+
+Clone this repository to your desired folder:
+
+cd my-folder
+
+git clone [repo-link](https://github.com/Kwaku28/architectural-summit.git)
+
+<!-- AUTHORS -->
+
+## 👥 Authors <a name="authors"></a>
+
+👤 **Lawrence Amoafo**
+
+- GitHub: [@Kwaku28](https://github.com/Kwaku28)
+- Twitter: [@kwakuamoafo](https://twitter.com/kwakuamoafo)
+- LinkedIn: [lawrenceamoafo](https://linkedin.com/in/lawrenceamoafo)
+
+<!-- FUTURE FEATURES -->
+
+## 🔭 Future Features <a name="future-features"></a>
+
+- [ ] **[Responsiveness]**
+
+<!-- CONTRIBUTING -->
+
+## 🤝 Contributing <a name="contributing"></a>
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+<!-- SUPPORT -->
+
+## ⭐️ Show your support <a name="support"></a>
+
+If you like this project show your love by leaving a comment
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+I would like to thank all Micronauts for their support
+
+Original design idea by [Cindy Shin in Behance](https://www.behance.net/adagio07).
+
+<!-- LICENSE -->
+
+## 📝 License <a name="license"></a>
+
+This project is [MIT](https://github.com/Kwaku28/architectural-summit/blob/main/LICENSE) licensed.
+
--- script.js
+++ script.js
@@ -1,6 +1,114 @@
-const moreButton = document.querySelector('.seeMore');
-const hiddenPort = document.querySelector('.hiddenPort');
+// Sliding menu
+const grid = document.querySelector('.grid');
+const slidingMenu = document.querySelector('.slidingMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const anchorTags = document.querySelectorAll('.slidingMenu ul > li');
 
-moreButton.addEventListener('click', () => {
-  hiddenPort.style.display = 'block';
+grid.addEventListener('click', () => {
+  slidingMenu.style.left = '0%';
 });
+
+closeMenu.addEventListener('click', () => {
+  slidingMenu.style.left = '100%';
+});
+
+anchorTags.forEach((sync) => sync.addEventListener('click', () => {
+  slidingMenu.style.left = '100%';
+}));
+
+// Render Feature Section Dynamically
+
+const speakers = [
+  {
+    id: '1',
+    name: 'Samuel Duodo',
+    title: 'Director of Art Centre Accra, Ghana',
+    experience: 'He stands gifted as one of the co-founders of Apple with an awesome portfolio in the geopolitical diaspora as well.',
+    picture: 'Images/portrait-real-black-african-man-260nw-505311127.jpg',
+  },
+  {
+    id: '2',
+    name: 'Anita Ama',
+    title: 'Minister of Education Nairobi, Kenya',
+    experience: 'With a dazzling expertise in the field of Education she has left her mark on the phase of Education as we know it in Africa.',
+    picture: 'Images/istockphoto-1060680104-612x612.jpg',
+  },
+  {
+    id: '3',
+    name: 'Lawrence Amoafo',
+    title: 'Prime Minister of United States',
+    experience: 'He stands gifted as one of the co-founders of Apple with an awesome portfolio in the geopolitical diaspora as well.',
+    picture: 'Images/istockphoto-502581380-612x612.jpg',
+  },
+  {
+    id: '4',
+    name: 'Anita Ama',
+    title: 'Minister of Education Nairobi, Kenya',
+    experience: 'With a dazzling expertise in the field of Education she has left her mark on the phase of Education as we know it in Africa.',
+    picture: 'Images/istockphoto-1060680104-612x612.jpg',
+  },
+];
+
+const loadSpeakers = () => {
+  const guestSpeakers = document.querySelector('.featureContainer');
+  guestSpeakers.innerHTML = '';
+  speakers.forEach((speaker) => {
+    const list = document.createElement('div');
+    if (speaker.id === '1' || speaker.id === '2') {
+      list.className = 'SpeakerPort';
+    } else {
+      list.className = 'SpeakerPort';
+      list.classList.add('hiddenPort');
+    }
+
+    list.innerHTML = `
+    <ion-icon id="picdec" name="film"></ion-icon> 
+    <img
+      src="${speaker.picture}"
+      alt="Black American Speaker"
+    />
+    <div class="port">
+      <span>
+        <b>${speaker.name}</b> <br />
+        <span class="portTitle"
+          >${speaker.title}</span
+        >
+        <br /></span
+      ><br />
+      <hr />
+      <br />
+      <span
+        >${speaker.experience}</span
+      >
+    </div>
+    `;
+    guestSpeakers.appendChild(list);
+  });
+};
+
+window.onload = () => {
+  loadSpeakers();
+};
+
+const more = document.querySelector('.seeMore');
+const less = document.querySelector('.seeLess');
+const panelSpeakers = document.getElementsByClassName('SpeakerPort');
+
+const openMore = () => {
+  for (let i = 0; i < panelSpeakers.length; i += 1) {
+    panelSpeakers[i].classList.remove('hiddenPort');
+  }
+  more.classList.add('active');
+  less.classList.add('active');
+};
+
+const closeMore = () => {
+  for (let i = 2; i < panelSpeakers.length; i += 1) {
+    panelSpeakers[i].classList.add('hiddenPort');
+  }
+  more.classList.remove('active');
+  less.classList.remove('active');
+};
+
+more.addEventListener('click', openMore);
+less.addEventListener('click', closeMore);
